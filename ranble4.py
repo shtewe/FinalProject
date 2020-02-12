07923f3fbabf13e3d0f9442fc4344c0f3d056884
Ahmad Shtewe <shteweahmad95@gmail.com>
2020-02-12 12:09:47 +0200

    add ranble4.py

---
+++ ranble4.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import serial
+import lcddriver
+from time import sleep
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+DisConPin=7
+GPIO.setup(DisConPin, GPIO.OUT)
+display=lcddriver.lcd()
+
+display.lcd_display_string("The Target Range",1)
+
+ser = serial.Serial ("/dev/ttyS0",9600) #Open port with baud rate
+#ser.write("AT+RENEW")
+#sleep(1)
+ser.write("AT+RESET")
+sleep(0.03)
+ser.write("AT+IMME1")
+sleep(0.03)
+ser.write("AT+ROLE1")
+sleep(0.03)
+ser.write("AT+PSWD123456")
+sleep(0.03)
+AvergSize=5
+data=[0 for i in xrange(AvergSize)]
+count=0
+Averg=40
+DisConFlag=0
+ConFlag=0
+TimeOut=0
+GPIO.output(DisConPin,False)
+while True :
+    received_data =ser.read()           #read serial port
+    sleep(0.03)
+    data_left =ser.inWaiting()          #check for remaining byte
+    received_data += ser.read(data_left)
+   # temp=received_data.strip()
+    Find_Index=received_data.find('OK+DIS0:')
+    print(Find_Index)
+    print(received_data)
+  #  if ConFlag :
+   #    if received_data.find('DONE')!=-1:
+    #      ConFlag=0
+     #     GPIO.output(7,False)
+
+    #else:
+    DisConFlag=(received_data.find('START1')!=-1)
+    if DisConFlag:
+      GPIO.output(DisConPin,True)
+      sleep(0.03)
+      GPIO.output(DisConPin,False)
+      DisConFlag=0
+    #  ConFlag=0
+
+ 
+    if (Find_Index!=-1):
+      Addres=received_data[8+Find_Index:20+Find_Index] 
+     # print( Addres,'3CA308A0264D',Addres=='3CA308A0264D')
+     # rssi=received_data[28+Find_Index:32+Find_Index]
+     # print("ADDRESS: " +Addres+"\n")
+     # print("RSSI: "+rssi+"\n")
+      if Addres=='3CA308A0264D':
+        # sleep(1)
+         #ser.write("AT+CON3CA308A0264D")
+         #sleep(0.03)
+       ##  ConFlag=1
+         RSSI=received_data[28+Find_Index:32+Find_Index]
+         data.pop(0)
+         try:
+           IntRSSI=int(RSSI)
+         except ValueError:
+           IntRSSI=int(Averg)
+         PrivData=int(data[-1])
+         if (((IntRSSI- PrivData >8)or(PrivData-IntRSSI>8))and (Averg>55)):
+           IntRSSI=int(Averg)
+         data.append(IntRSSI)
+         print("ADDRESS: " +Addres+"\n")
+         Averg=sum(data)/AvergSize
+         print("RSSI: %d\n"%(Averg))
+         display.ShowLCD_BarGraph(Averg,-120,5,2)
+   # write_data=input("Please enter a command AT as string:\n")
+   # print (write_data)
+    if not  ConFlag:
+      ser.write("AT+DISC?")              #transmit data serially
+     # ser.write("AT+CON3CA308A0264D")
+    else:
+     # ConFlag=0
+     if count ==1:
+        ser.write("AT+CON3CA308A0264D")
+        count=0
+        ConFlag=0
+     else:
+        count+=1
+     
+
+    sleep(1)
