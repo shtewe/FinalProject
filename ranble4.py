db6db84bf4772a4fb37c403ea312a13dcbf393e3
Ahmad Shtewe <shteweahmad95@gmail.com>
2020-02-12 15:22:35 +0200

    add finble func

--- ranble4.py
+++ ranble4.py
@@ -4,14 +4,17 @@ import serial
 import lcddriver
 from time import sleep
 import RPi.GPIO as GPIO
+ 
+GPIO.setwarnings(False)
+
 GPIO.setmode(GPIO.BOARD)
 DisConPin=7
 GPIO.setup(DisConPin, GPIO.OUT)
 display=lcddriver.lcd()
-
+#GPIO.setwarnings(False)
 display.lcd_display_string("The Target Range",1)
 
-ser = serial.Serial ("/dev/ttyS0",9600) #Open port with baud rate
+ser = serial.Serial ("/dev/ttyS0",9600, timeout=2) #Open port with baud rate
 #ser.write("AT+RENEW")
 #sleep(1)
 ser.write("AT+RESET")
@@ -25,28 +28,53 @@ sleep(0.03)
 AvergSize=5
 data=[0 for i in xrange(AvergSize)]
 count=0
-Averg=40
+Averg=-40
+MinAverg=-50
 DisConFlag=0
 ConFlag=0
 TimeOut=0
+Dic_Addr_Rssi={}
 GPIO.output(DisConPin,False)
+Addres1='3CA308A0264D'
+
+
+
+def FindBLE(StrInput):
+    dic={}
+    temp=StrInput.split('OK+DIS0:')
+    for  i in range(1,len(temp)):
+       dic[temp[i][:12]]=temp[i][20:24]
+    return dic
+
+
+
+
+
+
+
+
+
+
 while True :
-    received_data =ser.read()           #read serial port
+  
+    received_data =ser.read()           #read serial por
     sleep(0.03)
     data_left =ser.inWaiting()          #check for remaining byte
     received_data += ser.read(data_left)
-   # temp=received_data.strip()
+     # temp=received_data.strip()
     Find_Index=received_data.find('OK+DIS0:')
-    print(Find_Index)
-    print(received_data)
+   # print(Find_Index)
+    #print(received_data.split())
   #  if ConFlag :
    #    if received_data.find('DONE')!=-1:
     #      ConFlag=0
      #     GPIO.output(7,False)
 
     #else:
-    DisConFlag=(received_data.find('START1')!=-1)
+    rcevRssi=received_data.find('START1')
+    DisConFlag=(rcevRssi!=-1)
     if DisConFlag:
+      print("Iam rcevRssi: "+received_data[rcevRssi+6:rcevRssi+9])
       GPIO.output(DisConPin,True)
       sleep(0.03)
       GPIO.output(DisConPin,False)
@@ -55,27 +83,31 @@ while True :
 
  
     if (Find_Index!=-1):
-      Addres=received_data[8+Find_Index:20+Find_Index] 
+     # print(received_data.split('OK+DIS0:'))
+      Dic_Addr_Rssi= FindBLE(received_data)
+     # print(Dic_Addr_Rssi)
+      #Addres=received_data[8+Find_Index:20+Find_Index] 
      # print( Addres,'3CA308A0264D',Addres=='3CA308A0264D')
      # rssi=received_data[28+Find_Index:32+Find_Index]
      # print("ADDRESS: " +Addres+"\n")
      # print("RSSI: "+rssi+"\n")
-      if Addres=='3CA308A0264D':
+      if Addres1 in Dic_Addr_Rssi:
         # sleep(1)
          #ser.write("AT+CON3CA308A0264D")
          #sleep(0.03)
-       ##  ConFlag=1
-         RSSI=received_data[28+Find_Index:32+Find_Index]
+         ConFlag=1
+         #RSSI=received_data[28+Find_Index:32+Find_Index]
+         RSSI=Dic_Addr_Rssi[Addres1]
          data.pop(0)
          try:
            IntRSSI=int(RSSI)
          except ValueError:
            IntRSSI=int(Averg)
          PrivData=int(data[-1])
-         if (((IntRSSI- PrivData >8)or(PrivData-IntRSSI>8))and (Averg>55)):
+         if (((IntRSSI- PrivData >8)or(PrivData-IntRSSI>8))and (Averg<MinAverg)):
            IntRSSI=int(Averg)
          data.append(IntRSSI)
-         print("ADDRESS: " +Addres+"\n")
+         print("ADDRESS: " +Addres1+"\n")
          Averg=sum(data)/AvergSize
          print("RSSI: %d\n"%(Averg))
          display.ShowLCD_BarGraph(Averg,-120,5,2)
@@ -87,7 +119,7 @@ while True :
     else:
      # ConFlag=0
      if count ==1:
-        ser.write("AT+CON3CA308A0264D")
+        ser.write("AT+CON"+Addres1)
         count=0
         ConFlag=0
      else:
@@ -95,3 +127,5 @@ while True :
      
 
     sleep(1)
+
+
